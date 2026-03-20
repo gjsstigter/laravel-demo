5d326ceab8bf8ccb5b5588d6040916aad1e044a4
gjsstigter <30732869+gjsstigter@users.noreply.github.com>
2026-03-20 08:17:35 +0100

    Fix syntax error in middleware trustProxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
             Request::HEADER_X_FORWARDED_FOR |
             Request::HEADER_X_FORWARDED_HOST |
             Request::HEADER_X_FORWARDED_PORT |
-            Request::HEADER_X_FORWARDED_PROTO, at: '*');
+            Request::HEADER_X_FORWARDED_PROTO, at: '*'
         );
         
     })
